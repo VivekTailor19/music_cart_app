058f51cee9ecf8b864004c61e597caba7be68d7e
Vivek <tailorvivekgec19@gmail.com>
2023-04-19 14:05:00 +0530

    music item cart app

--- lib/screen/provider/music_provider.dart
+++ lib/screen/provider/music_provider.dart
@@ -6,7 +6,7 @@ import 'package:music_cart_app/screen/modal/music_modal.dart';
 class MusicProvider extends ChangeNotifier
 {
     List<Music> itemlist = [
-        Music(photo: "assets/images/casio.jpg",name: "Casio",detail: "YAMAHA PSR-F52 PORTABLE KEYBOARD WITH 61 KEYS",price: 7200),
+        Music(photo: "assets/images/casio.jpg",name: "Casio",detail: "YAMAHA PSR-F52 PORTABLE KEYBOARD WITH 61 KEYS",price: 7200,qty: 1),
         Music(photo: "assets/images/damru.jpg",name: "Damru",detail: "De Kulture Works™ Hand Hammered Brass Damru Mini Hand Drum Musical Instruments 4X3 DH (Inches)",price: 1475,qty: 1),
         Music(photo: "assets/images/gitar.jpg",name: "Gitar",detail: "Kadence Wanderer Series ukulele + (Free online learning Course) Brown Mahogany wood Ukulele With Bag",price: 2199,qty: 1),
         Music(photo: "assets/images/harmonium.jpg",name: "Harmonium",detail: "Laying Style™ 9 Stopper 42 Key 2 Reed 440 Tuned Total Wood Musical Instrument Harmonium (Brown)",price: 7700,qty: 1),
