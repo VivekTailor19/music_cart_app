fb76ec790725be9fb2fd604aeebcbf63e4640eaa
Vivek <tailorvivekgec19@gmail.com>
2023-04-20 13:34:54 +0530

    music item cart app

--- lib/screen/provider/music_provider.dart
+++ lib/screen/provider/music_provider.dart
@@ -6,26 +6,25 @@ import 'package:music_cart_app/screen/modal/music_modal.dart';
 class MusicProvider extends ChangeNotifier
 {
     List<Music> itemlist = [
-        Music(photo: "assets/images/casio.png",name: "Casio",detail: "YAMAHA PSR-F52 PORTABLE KEYBOARD WITH 61 KEYS",price: 7200,qty: 1),
-        Music(photo: "assets/images/damru.png",name: "Damru",detail: "De Kulture Works™ Hand Hammered Brass Damru Mini Hand Drum Musical Instruments 4X3 DH (Inches)",price: 1475,qty: 1),
-        Music(photo: "assets/images/gitar.png",name: "Gitar",detail: "Kadence Wanderer Series ukulele + (Free online learning Course) Brown Mahogany wood Ukulele With Bag",price: 2199,qty: 1),
-        Music(photo: "assets/images/harmonium.png",name: "Harmonium",detail: "Laying Style™ 9 Stopper 42 Key 2 Reed 440 Tuned Total Wood Musical Instrument Harmonium (Brown)",price: 7700,qty: 1),
-        Music(photo: "assets/images/manjeera.png",name: "Manjeera",detail: "Berua Karatals for kirtan, Spiritual Music Musical Instruments Indian Manjeera",price: 350,qty: 1),
-        Music(photo: "assets/images/mouthorgan.png",name: "Mouth Organ",detail: "East top Chromatic Mouth Organs Harmonica Key of Bb,12 Holes 48 Tones Chromatic Harmonica Mouth Organ Musical Instrument for Adults and Professionals(Bb)",price: 8910,qty: 1),
-        Music(photo: "assets/images/tabla.png",name: "Tabla",detail: "HRB MUSICALS® Student Table Jodi, Basic Table Set, Steel Bayan, Dayan wooden table, Hammer, Cushions & Cover, with bag- Perfect Tablas for Students and Beginners on Budget Tabla, Indian Hand Drums",price: 6555,qty: 1),
-        Music(photo: "assets/images/dholak.png",name: "Dholak",detail: "Ganesh Musical Store Presents - Handmade Dholak",price:1500,qty: 1),
-        Music(photo: "assets/images/flute.png",name: "Flute",detail: "Shiv'z Muzic Flute, G Natural BASE, PREMIUM Bansuri, 25 inches (with Written Manual & Bag)",price: 2500,qty: 1),
-        Music(photo: "assets/images/shenai.png",name: "Shenai",detail: "SAONA India Indian Classical Wind Musical Instrument Shehnai",price: 1600,qty: 1),
-        Music(photo: "assets/images/tumbi.png",name: "Tumbi",detail: "SAI Musical Pure Handmade Fine Crafted Indian Musical Instruments Wooden Playing Iktara (Tumbi)",price: 650,qty: 1),
+        Music(photo: "assets/images/casio.jpg",name: "Casio",detail: "YAMAHA PSR-F52 PORTABLE KEYBOARD WITH 61 KEYS",price: 7200,qty: 1),
+        Music(photo: "assets/images/damru.jpg",name: "Damru",detail: "De Kulture Works™ Hand Hammered Brass Damru Mini Hand Drum Musical Instruments 4X3 DH (Inches)",price: 1475,qty: 1),
+        Music(photo: "assets/images/gitar.jpg",name: "Gitar",detail: "Kadence Wanderer Series ukulele + (Free online learning Course) Brown Mahogany wood Ukulele With Bag",price: 2199,qty: 1),
+        Music(photo: "assets/images/harmonium.jpg",name: "Harmonium",detail: "Laying Style™ 9 Stopper 42 Key 2 Reed 440 Tuned Total Wood Musical Instrument Harmonium (Brown)",price: 7700,qty: 1),
+        Music(photo: "assets/images/manjeera.jpg",name: "Manjeera",detail: "Berua Karatals for kirtan, Spiritual Music Musical Instruments Indian Manjeera",price: 350,qty: 1),
+        Music(photo: "assets/images/mouthorgan.jpg",name: "Mouth Organ",detail: "East top Chromatic Mouth Organs Harmonica Key of Bb,12 Holes 48 Tones Chromatic Harmonica Mouth Organ Musical Instrument for Adults and Professionals(Bb)",price: 8910,qty: 1),
+        Music(photo: "assets/images/tabla.jpg",name: "Tabla",detail: "HRB MUSICALS® Student Table Jodi, Basic Table Set, Steel Bayan, Dayan wooden table, Hammer, Cushions & Cover, with bag- Perfect Tablas for Students and Beginners on Budget Tabla, Indian Hand Drums",price: 6555,qty: 1),
+        Music(photo: "assets/images/dholak.jpg",name: "Dholak",detail: "Ganesh Musical Store Presents - Handmade Dholak",price:1500,qty: 1),
+        Music(photo: "assets/images/flute.jpg",name: "Flute",detail: "Shiv'z Muzic Flute, G Natural BASE, PREMIUM Bansuri, 25 inches (with Written Manual & Bag)",price: 2500,qty: 1),
+        Music(photo: "assets/images/shenai.jpg",name: "Shenai",detail: "SAONA India Indian Classical Wind Musical Instrument Shehnai",price: 1600,qty: 1),
+        Music(photo: "assets/images/tumbi.jpg",name: "Tumbi",detail: "SAI Musical Pure Handmade Fine Crafted Indian Musical Instruments Wooden Playing Iktara (Tumbi)",price: 650,qty: 1),
 
     ];
 
-    List<Music> cartlist = [Music(photo: "assets/images/tumbi.png",name: "Tumbi",detail: "SAI Musical Pure Handmade Tumbi",price: 650,qty: 1),];
+    List<Music> cartlist = [Music(photo: "assets/images/tumbi.jpg",name: "Tumbi",detail: "SAI Musical Pure Handmade Tumbi",price: 650,qty: 1),];
 
     int total = 0;
     int finalprice = 0;
     int delivery = 100;
-
 
 
     void increase(int index)
--- lib/screen/view/cart_ui.dart
+++ lib/screen/view/cart_ui.dart
@@ -24,69 +24,70 @@ class _Cart_UIState extends State<Cart_UI> {
 
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text("Shopping Area",style: TextStyle(fontSize: 25),),),
+        appBar: AppBar(title: Text("Shopping Area",style: TextStyle(fontSize: 25),),elevation:5,),
         body: Center(
           child: Column(
             children: [
+              SizedBox(height: 14,),
               Expanded(
                 child: ListView.builder(
                   itemCount: providerF!.cartlist.length,
                   itemBuilder: (context, index) {
                     return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 5),
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 7),
                       child: Container(height: 110,
-                        decoration: BoxDecoration(color: Colors.teal,
+                        decoration: BoxDecoration(
+                          color: Color(0x08666666),
                             borderRadius: BorderRadius.circular(20)),
                         child: Row(
                           children: [
                             Container(height: 110,width: 110,
-                                decoration: BoxDecoration(color: Colors.tealAccent,
+                                decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(20),
                                     image: DecorationImage(image: AssetImage(
                                         "${providerF!.cartlist[index].photo}"),fit: BoxFit.fill))
                             ),
                             Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10),
-                              child: Container(
-
-                                child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Text("${providerF!.cartlist[index].name}",),
-                                    Text("${providerT!.cartlist[index].price! * providerT!.cartlist[index].qty!}",),
-
-                                  ],
-                                ),
-
-                              ),
-                            ),
-                            Spacer(),
-                            Container(
+                              padding: const EdgeInsets.only(top: 15,left: 5,bottom: 15),
                               child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.end,
+
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  IconButton(onPressed: (){
-
-                                    providerF!.delete(index);
-
-                                  }, icon: Icon(Icons.close_rounded,size: 12)),
-                                  Row(
-                                    children: [
-                                      IconButton(onPressed: (){
-                                        providerF!.decrease(index);
-                                      }, icon: Icon(Icons.remove_circle_outline_rounded)),
-
-                                      Container(height: 30,width: 30,alignment: Alignment.center,
-                                          child: Text("${providerT!.cartlist[index].qty}",style: TextStyle(fontSize: 25,color: Colors.pinkAccent),)),
-
-                                      IconButton(onPressed: (){
-                                        providerF!.increase(index);
-                                      }, icon: Icon(Icons.add_circle_outline_rounded)),
-                                    ],
-
-                                  )
+                                  Text("${providerF!.cartlist[index].name}",style: TextStyle(fontSize: 16),),
+                                  Text("₹${providerT!.cartlist[index].price}",style: TextStyle(fontSize: 12.5),),
+                                  Spacer(),
+                                  Text("Total: ₹${providerT!.cartlist[index].price! * providerT!.cartlist[index].qty!}",style: TextStyle(fontSize: 15),),
 
                                 ],
                               ),
+                            ),
+                            Spacer(),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                IconButton(onPressed: (){
+
+                                  providerF!.delete(index);
+
+                                }, icon: Icon(Icons.close_rounded,size: 20)),
+                                Spacer(),
+                                Row(mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(onPressed: (){
+                                      providerF!.decrease(index);
+                                    }, icon: Icon(Icons.remove_circle_outline_outlined)),
+
+                                    Container(height: 18,width: 18,alignment: Alignment.center,
+                                        child: Text("${providerT!.cartlist[index].qty}",style: TextStyle(fontSize: 15,color: Colors.pinkAccent),)),
+
+                                    IconButton(onPressed: (){
+                                      providerF!.increase(index);
+                                    }, icon: Icon(Icons.add_circle_outlined)),
+                                  ],
+
+                                )
+
+                              ],
                             ),
 
                           ],
@@ -98,14 +99,53 @@ class _Cart_UIState extends State<Cart_UI> {
                 ),
               ),
 
-              SizedBox(height: 50,),
-              ElevatedButton(onPressed: (){
-                providerF!.finalbill();
-              }, child: Text("Create Bill")),
+              Container(height: 200,color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("SubTotal Items(${providerF!.cartlist.length})",style: TextStyle(fontSize: 13,color: Colors.black38),),
+                          Text("₹ ${providerT!.total}.00",style: TextStyle(fontSize: 18),)
+                        ],
+                      ),
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Delievery Fee",style: TextStyle(fontSize: 13,color: Colors.black38),),
+                          Text("₹ ${providerT!.delivery}.00")
+                        ],
+                      ),
 
-              Text("Total ${providerT!.total}",style: TextStyle(fontSize: 30,color: Colors.pink),),
-              Text("Delivery ${providerT!.delivery}",style: TextStyle(fontSize: 30,color: Colors.pink),),
-              Text("Final Price ${providerT!.finalprice}",style: TextStyle(fontSize: 30,color: Colors.pink),)
+                      Divider(thickness: 1.5,color: Colors.black26),
+
+                      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Total",style: TextStyle(fontSize: 15,color: Colors.black38),),
+                          Text("₹ ${providerT!.finalprice}.00",style: TextStyle(fontSize: 20))
+                        ],
+                      ),
+                      Container(height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Color(0xff131313),
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Text("Go to Payment",style: TextStyle(fontSize: 18,color: Colors.white),),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+
+              ),
+              // ElevatedButton(onPressed: (){
+              //   providerF!.finalbill();
+              // }, child: Text("Create Bill")),
+              //
+              // Text("Total ${providerT!.total}",style: TextStyle(fontSize: 30,color: Colors.pink),),
+              // Text("Delivery ${providerT!.delivery}",style: TextStyle(fontSize: 30,color: Colors.pink),),
+              // Text("Final Price ${providerT!.finalprice}",style: TextStyle(fontSize: 30,color: Colors.pink),)
 
 
             ],),
